8a6851781c6c8ad704fd52923b7489d9233cd26c
Nicolas Forney <nicolas@eforney.com>
2019-02-01 23:07:17 +0100

    feat: add first transferwise csv import feature

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const Excel = require('exceljs');
 const program = require('commander');
 const fs = require('fs');
 const moment = require('moment');
-
-let filenameValue;
+const axios = require('axios');
 
 program
     .version('1.0.0')
@@ -14,25 +13,99 @@ program
 program
     .command('post <filename>')
     .alias('p')
-    .description('import Post statement')
+    .description('import Post statements')
     .action(filename => {
-        filenameValue = filename;
+        readPostCsvFile(filename)
     });
+
+program
+    .command('transferwise')
+    .alias('t')
+    .description('import Transferwise statements')
+    .action((env) => {
+        generateTransferwiseCsv()
+    })
 
 
 program.parse(process.argv);
 
-// Remove carriage return from initial csv file
-fs.readFile(filenameValue, 'latin1', function (err, data) {
-    if (err) {
-        return console.log(err);
+
+function generateTransferwiseCsv() {
+    const transferwiseConfigs = JSON.parse(readTransferwiseConfig());
+
+    for (const config of transferwiseConfigs) {
+        getTransactions(config).then(response => {
+            let {
+                transactions,
+                accountHolder: {
+                    firstName,
+                    lastName
+                }
+            } = response.data;
+
+            const filename = ('transferwise-' + firstName + '-' + lastName + '.csv').toLowerCase();
+            createTransferwiseCsv(transactions, filename)
+        });
     }
-    let result = data.replace(/(.*)\r/gm, '$1');
-    fs.writeFile(filenameValue, result, 'latin1', function (err) {
-        if (err) return console.log(err);
-        createResultFile();
+
+}
+
+function createTransferwiseCsv(transactions, outputFilename) {
+    let fileContent = 'Memo,Inflow,Outflow,Date\n';
+    for (const transaction of transactions) {
+        let {
+            amount: {
+                value: amount
+            },
+            date,
+            details: {
+                description
+            },
+            totalFees: {value: fees},
+            type
+        } = transaction;
+
+        description = removeComma(description)+' (fees: '+fees+')';
+
+        date = moment(date).format('YYYY-MM-DD')
+        let inflow = '', outflow = '';
+        if (type === 'DEBIT') {
+            outflow = Math.abs(amount).toString();
+        } else {
+            inflow = amount.toString();
+        }
+        fileContent += description + ',' + inflow + ',' + outflow + ',' + date + '\n';
+    }
+    fs.writeFile(outputFilename, fileContent, function (err) {
+        if (err) throw err;
     });
-});
+}
+
+function getTransactions(config) {
+    let url = 'https://api.transferwise.com/v1/borderless-accounts/' + config.accountId + '/statement.json?currency=' + config.currency + '&intervalStart=2019-01-01T00:00:00.000Z&intervalEnd=2019-02-15T23:59:59.999Z';
+    return axios.get(url, {headers: {Authorization: 'Bearer ' + config.token}})
+}
+
+function readTransferwiseConfig() {
+    return fs.readFileSync('transferwise.config.json', 'utf8');
+}
+
+function readPostCsvFile(filename) {
+// Remove carriage return from initial csv file
+    fs.readFile(filename, 'latin1', function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        //Remove carriage returns
+        let result = data.replace(/(.*)\r/gm, '$1');
+        //Remove comma
+        result = removeComma(result);
+        fs.writeFile(filename, result, 'latin1', function (err) {
+            if (err) return console.log(err);
+            createResultFile();
+        });
+    });
+}
 
 function createResultFile() {
     let workbook = new Excel.Workbook();
@@ -40,8 +113,8 @@ function createResultFile() {
         delimiter: ';'
     }
 
-    let data = fs.readFileSync(filenameValue, {encoding: 'latin1'}).toString();
-    let filenameLatin = filenameValue.replace('.csv', 'Latin1.csv');
+    let data = fs.readFileSync(filename, {encoding: 'latin1'}).toString();
+    let filenameLatin = filename.replace('.csv', 'Latin1.csv');
     fs.writeFileSync(filenameLatin, data);
 
     workbook.csv.readFile(filenameLatin, CSVoptions)
@@ -103,7 +176,7 @@ function createResultFile() {
                         if (err) {
                             return console.log(err);
                         }
-                        let result = data.replace(/^"(.*)",/gm, '$1');
+                        let result = data.replace(/^"(.*)"[\s]*,/gm, '$1,');
                         fs.writeFile(resultFile, result, 'utf8', function (err) {
                             if (err) return console.log(err);
                         });
@@ -111,4 +184,8 @@ function createResultFile() {
                 });
         });
 
+}
+
+function removeComma(str) {
+    return str.replace(/,\s|\s,|,/gm, ' ');
 }
